8411a79a1fa1872840c097877b9a84859964cd7d
TLantern <pgtherealmvp@gmail.com>
2025-04-17 22:41:02 -0600

    Update login-form.tsx

--- block-frontend/src/components/login-form.tsx
+++ block-frontend/src/components/login-form.tsx
@@ -18,7 +18,7 @@ export function LoginForm({
       <div className="grid gap-6">
         <div className="grid gap-3">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Input id="email" type="email" placeholder="blockdev@example.com" required />
         </div>
         <div className="grid gap-3">
           <div className="flex items-center">
