d000905543471fe1846220272b3cfb70e34af72e
boltpeer <boltpeer@gmail.com>
2025-04-28 16:01:44 +0100

    Initial commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,32 +12,33 @@
 </head>
 
 <body>
+  <div class="max-w-xl mx-auto ">
+    <form method="POST" action="{{ route('login') }}">
+      @csrf
 
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-
-    <!-- Email Address -->
-    <div>
+      <!-- Email Address -->
+      <div>
         <!-- <x-input-label for="email" :value="__('Email')" /> -->
         <!-- <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" /> -->
         <!-- <x-input-error :messages="$errors->get('email')" class="mt-2" /> -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
+      </div>
+      <figure>
+        <img src="../../../assets/images/block-logo.png" alt="block-logo">
+      </figure>
+      <h1>Log in</h1>
+      <label for="email">Email address</label>
+      <input type="email" name="email" id="email" placeholder="example@block.com" required>
+      <x-input-error :messages="$errors->get('email')" class="mt-2" />
+      <label for="password">Your password</label>
+      <input type="password" name="password" id="password" placeholder="Password" required><br><br>
+      <a href="/frontend/pages/recover-password.html">Forgotten password?</a>
+      <button type="submit">Log in</button>
+
+      <p>Don't have an account? <a href="{{ route('register') }}">Create account</a></p>
+    </form>
     </div>
-<figure>
-      <img src="../../../assets/images/block-logo.png" alt="block-logo">
-    </figure>
-    <h1>Log in</h1>
-    <label for="email">Email address</label>
-    <input type="email" name="email" id="email" placeholder="example@block.com" required>
-    <x-input-error :messages="$errors->get('email')" class="mt-2" />
-    <label for="password">Your password</label>
-    <input type="password" name="password" id="password" placeholder="Password" required><br><br>
-    <a href="/frontend/pages/recover-password.html">Forgotten password?</a>
-    <button type="submit">Log in</button> 
-    
-    <p>Don't have an account? <a href="{{ route('register') }}">Create account</a></p>
-  </form>
 </body>
 
 </html>
