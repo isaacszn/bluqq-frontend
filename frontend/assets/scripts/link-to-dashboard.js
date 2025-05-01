5b03200e47a7e87df57daeaf2352a45350f0cbaa
TLantern <pgtherealmvp@gmail.com>
2025-05-01 08:57:25 -0600

    new

---
+++ frontend/assets/scripts/link-to-dashboard.js
@@ -0,0 +1,7 @@
+document.querySelector('#register-btn').addEventListener('click', () => {
+  window.location.href = '/frontend/pages/register.html'
+})
+
+document.querySelector('#login-btn').addEventListener('click', () => {
+  window.location.href = '/frontend/pages/login.html'
+})
